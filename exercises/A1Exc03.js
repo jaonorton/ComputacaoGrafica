5323ddf83ff5e0c62a3cd8211f3bfa6b1910416a
João Pedro R. Norton <jaonorton>
2024-04-07 20:22:05 -0300

    Exercícios aula 1

---
+++ exercises/A1Exc03.js
@@ -0,0 +1,60 @@
+import * as THREE from  'three';
+import { OrbitControls } from '../build/jsm/controls/OrbitControls.js';
+import {initRenderer, 
+        initCamera,
+        initDefaultBasicLight,
+        setDefaultMaterial,
+        InfoBox,
+        onWindowResize,
+        createGroundPlaneXZ} from "../libs/util/util.js";
+
+let scene, renderer, camera, material, light, orbit, material2, material3; // Initial variables
+scene = new THREE.Scene();    // Create main scene
+renderer = initRenderer();    // Init a basic renderer
+camera = initCamera(new THREE.Vector3(0, 15, 30)); // Init camera in this position
+material = setDefaultMaterial("orange"); 
+
+
+
+light = initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
+orbit = new OrbitControls( camera, renderer.domElement ); // Enable mouse rotation, pan, zoom etc.
+
+// Listen window size changes
+window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
+
+// Show axes (parameter is size of each axis)
+let axesHelper = new THREE.AxesHelper( 12 );
+scene.add( axesHelper );
+
+// create the ground plane
+let plane = createGroundPlaneXZ(20, 20)
+scene.add(plane);
+
+// create a cube
+let cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
+
+for ( let x = -8; x <=8; x = x+8){
+  for (let z = -8; z<=8; z = z+8){
+    let cube = new THREE.Mesh(cubeGeometry, material);
+    cube.position.set(x, 2.0, z);
+    scene.add(cube);
+  }
+}
+
+
+// Use this to show information onscreen
+let controls = new InfoBox();
+  controls.add("Basic Scene");
+  controls.addParagraph();
+  controls.add("Use mouse to interact:");
+  controls.add("* Left button to rotate");
+  controls.add("* Right button to translate (pan)");
+  controls.add("* Scroll to zoom in/out.");
+  controls.show();
+
+render();
+function render()
+{
+  requestAnimationFrame(render);
+  renderer.render(scene, camera) // Render scene
+}
