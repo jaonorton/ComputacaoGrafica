dca4c0defa67f92c2254a9de5f08eb8c1be86ceb
jaonorton <jaonorton@gmail.com>
2024-09-09 16:04:13 -0300

    Exc02 Quase feito

--- exercises/Aula10/Exc02.js
+++ exercises/Aula10/Exc02.js
@@ -1,10 +1,4 @@
-/*
-Based on the following example:
-http://jyunming-chen.github.io/tutsplus/tutsplus28.html
-*/
-
 import * as THREE from  'three';
-import GUI from '../../libs/util/dat.gui.module.js'
 import {OrbitControls} from '../../build/jsm/controls/OrbitControls.js';
 import {initRenderer, 
 		initCamera,
@@ -35,6 +29,36 @@ var groundPlane = createGroundPlane(100.0, 100.0, 100, 100); // width and height
    groundPlane.position.y = -7.6;
    groundPlane.rotation.x = -0.5 * Math.PI;
 scene.add(groundPlane);
+
+
+// Create main object
+let geom = new THREE.SphereGeometry(3, 64, 64);
+let colormap = 	new THREE.TextureLoader().load("../../assets/textures/displacement/rockWall.jpg");
+    colormap.colorSpace = THREE.SRGBColorSpace;
+let normalmap = new THREE.TextureLoader().load("../../assets/textures/displacement/rockWall_Normal.jpg");
+let dispmap = 	new THREE.TextureLoader().load("../../assets/textures/displacement/rockWall_Height.png");
+
+let mat = new THREE.MeshStandardMaterial({
+	side: THREE.DoubleSide,
+	color:"white",
+	map: colormap,
+	normalMap: normalmap,
+	displacementMap: dispmap,
+	displacementScale: 0.2,
+});
+mat.normalScale.set(0.2, 0.2);
+
+let mesh = new THREE.Mesh(geom, mat);
+setTextureOptions(mesh.material, 4, 3); // Set repeat and wrapping modes
+function setTextureOptions(material, repu, repv){
+	material.map.repeat.set(repu,repv);
+	material.displacementMap.repeat.set(repu,repv);
+	material.normalMap.repeat.set(repu,repv);
+	
+	material.map.wrapS = material.displacementMap.wrapS = material.normalMap.wrapS = THREE.RepeatWrapping;
+	material.map.wrapT = material.displacementMap.wrapT = material.normalMap.wrapT = THREE.RepeatWrapping;	
+}
+scene.add(mesh);
 
 render();
 
